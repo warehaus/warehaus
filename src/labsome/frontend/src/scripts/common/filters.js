a3b2f1bfcb26fc5a029a8223ec0be58d1d1f4cdf
Zohar Zilberman <popen2@gmail.com>
2015-11-28 20:45:52 +0200

    Add isEmpty and isNotEmpty filters

--- src/labsome/frontend/src/scripts/common/filters.js
+++ src/labsome/frontend/src/scripts/common/filters.js
@@ -8,3 +8,29 @@ angular.module('labsome.common').filter('titlecase', function() {
         });
     };
 });
+
+var _is_empty = function(o) {
+    if (o === undefined || o === null) {
+        return true;
+    }
+
+    if (Array.isArray(o)) {
+        return o.length == 0;
+    }
+
+    if (typeof o === 'object') {
+        return Object.getOwnPropertyNames(o).length == 0;
+    }
+
+    return o;
+};
+
+angular.module('labsome.common').filter('isEmpty', function() {
+    return _is_empty;
+});
+
+angular.module('labsome.common').filter('isNotEmpty', function() {
+    return function(o) {
+        return !_is_empty(o);
+    };
+});
