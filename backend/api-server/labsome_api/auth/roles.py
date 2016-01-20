254f3a9c8d81f359a49f57bf8a199006833e4a0c
Zohar Zilberman <popen2@gmail.com>
2016-01-20 11:44:02 +0200

    Add non-decorators: require_{admin,user}

--- backend/api-server/labsome_api/auth/roles.py
+++ backend/api-server/labsome_api/auth/roles.py
@@ -4,27 +4,33 @@ from bunch import Bunch
 from flask import abort as flask_abort
 from flask_jwt import current_identity
 from flask_jwt import jwt_required
+
 roles = Bunch(
     Admin = 'admin',
     User = 'user',
 )
 
+@jwt_required()
 def _require_roles(*role_names):
     if set(role_names) - set(current_identity.roles):
         flask_abort(httplib.FORBIDDEN)
 
+def require_admin():
+    _require_roles(roles.Admin)
+
+def require_user():
+    _require_roles(roles.User)
+
 def admin_required(func):
     @wraps(func)
-    @jwt_required()
     def wrapper(*args, **kwargs):
-        _require_roles(roles.Admin)
+        require_admin()
         return func(*args, **kwargs)
     return wrapper
 
 def user_required(func):
     @wraps(func)
-    @jwt_required()
     def wrapper(*args, **kwargs):
-        _require_roles(roles.User)
+        require_user()
         return func(*args, **kwargs)
     return wrapper
