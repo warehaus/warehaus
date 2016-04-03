b6a8c71be9595c1f2e8c76e9b9bbc49c73a9ec5c
Zohar Zilberman <popen2@gmail.com>
2016-04-03 23:03:42 +0300

    Fix payload_handler in api-server to generate proper JWTs

--- backend/api-server/warehaus_api/auth/__init__.py
+++ backend/api-server/warehaus_api/auth/__init__.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 from datetime import timedelta
 from logging import getLogger
 from flask import request
+from flask import current_app
 from flask_jwt import JWT
 from .models import User
 from .roles import roles
@@ -25,8 +27,16 @@ def identify(payload):
         return None
     return User.query.get(jwt_subject)
 
+def payload_handler(identity):
+    iat = datetime.utcnow()
+    exp = iat + current_app.config.get('JWT_EXPIRATION_DELTA')
+    nbf = iat + current_app.config.get('JWT_NOT_BEFORE_DELTA')
+    identity = getattr(identity, 'id') or identity['id']
+    return {'exp': exp, 'iat': iat, 'nbf': nbf, 'sub': identity}
+
 def init_auth(app):
     app.config['JWT_AUTH_URL_RULE'] = '/api/v1/auth/login'
     app.config['JWT_EXPIRATION_DELTA'] = timedelta(days=7)
     app.config['JWT_AUTH_HEADER_PREFIX'] = 'JWT'
     jwt = JWT(app, authenticate, identify)
+    jwt.jwt_payload_handler(payload_handler)
