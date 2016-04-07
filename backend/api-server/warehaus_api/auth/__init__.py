a22355e96a47fb3c928492c2fb6d356b49d20f54
Zohar Zilberman <popen2@gmail.com>
2016-04-07 21:17:08 +0300

    Remove authentication leftovers from api-server

--- backend/api-server/warehaus_api/auth/__init__.py
+++ backend/api-server/warehaus_api/auth/__init__.py
@@ -8,17 +8,8 @@ from .models import User
 from .roles import roles
 from .roles import user_required
 from .roles import admin_required
-from .login import validate_user
 
 logger = getLogger(__name__)
-
-def authenticate(username, password):
-    try:
-        user = validate_user(username, password)
-        logger.info('Successfully authenticated {!r}'.format(username))
-        return user
-    except Exception as error:
-        logger.exception('Error authenticating {!r}'.format(username))
 
 def identify(payload):
     logger.debug('Got payload: {!r}'.format(payload))
@@ -35,8 +26,8 @@ def payload_handler(identity):
     return {'exp': exp, 'iat': iat, 'nbf': nbf, 'sub': identity}
 
 def init_auth(app):
-    app.config['JWT_AUTH_URL_RULE'] = '/api/v1/auth/login'
+    app.config['JWT_AUTH_URL_RULE'] = None
     app.config['JWT_EXPIRATION_DELTA'] = timedelta(days=7)
     app.config['JWT_AUTH_HEADER_PREFIX'] = 'JWT'
-    jwt = JWT(app, authenticate, identify)
+    jwt = JWT(app, None, identify)
     jwt.jwt_payload_handler(payload_handler)
