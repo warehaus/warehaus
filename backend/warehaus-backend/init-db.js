1c2a07d6c222579a67d3a8d68f2ee8a3231c8641
Zohar Zilberman <popen2@gmail.com>
2016-04-13 16:39:35 +0300

    Refactor init-db.js

--- backend/warehaus-backend/init-db.js
+++ backend/warehaus-backend/init-db.js
@@ -6,47 +6,55 @@ var db = require('./db');
 var r = require('rethinkdb');
 require('rethinkdb-init')(r);
 
+const settings_table = {
+    name: 'settings'
+};
+
+const users_table = {
+    name: 'user',
+    indexes: [
+        'username',
+        {
+            name: 'api_tokens',
+            multi: true
+        }
+    ]
+};
+
+const google_users_table = {
+    name: 'user_google'
+};
+
+const objects_table = {
+    name: 'object',
+    indexes: [
+        'slug',
+        'type_id',
+        'parent_id',
+        {
+            name: 'slug_parent',
+            indexFunction: [r.row('slug'), r.row('parent_id')]
+        },
+        {
+            name: 'slug_type',
+            indexFunction: [r.row('slug'), r.row('type_id')]
+        },
+        {
+            name: 'slug_type_parent',
+            indexFunction: [r.row('slug'), r.row('type_id'), r.row('parent_id')]
+        },
+        {
+            name: 'parent_type',
+            indexFunction: [r.row('parent_id'), r.row('type_id')]
+        }
+    ]
+};
+
 r.init(db.config(), [
-    {
-        name: 'settings'
-    },
-    {
-        name: 'user',
-        indexes: [
-            'username',
-            {
-                name: 'api_tokens',
-                multi: true
-            }
-        ]
-    },
-    {
-        name: 'user_google'
-    },
-    {
-        name: 'object',
-        indexes: [
-            'slug',
-            'type_id',
-            'parent_id',
-            {
-                name: 'slug_parent',
-                indexFunction: [r.row('slug'), r.row('parent_id')]
-            },
-            {
-                name: 'slug_type',
-                indexFunction: [r.row('slug'), r.row('type_id')]
-            },
-            {
-                name: 'slug_type_parent',
-                indexFunction: [r.row('slug'), r.row('type_id'), r.row('parent_id')]
-            },
-            {
-                name: 'parent_type',
-                indexFunction: [r.row('parent_id'), r.row('type_id')]
-            }
-        ]
-    }
+    settings_table,
+    users_table,
+    google_users_table,
+    objects_table
 ]).then(conn => {
     conn.close();
 }).catch(err => {
